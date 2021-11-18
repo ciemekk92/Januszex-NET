c4b396c2285b3b3b5741079769e1be6127b6fc73
ciemekk92 <przemyslaw.reducha@gmail.com>
2021-11-18 22:49:28 +0100

    Add dark mode toggle to profile page

---
+++ Januszex/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Januszex.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Januszex.Areas.Identity.Pages.Account.Manage
+{
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public IndexModel(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [Display(Name = "Nazwa u¿ytkownika")]
+        public string Username { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Phone]
+            [Display(Name = "Numer telefonu")]
+            public string PhoneNumber { get; set; }
+
+            [Display(Name = "Tryb ciemny")]
+            public bool DarkMode { get; set; }
+        }
+
+        private async Task LoadAsync(User user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var darkMode = user.DarkMode;
+
+            Username = userName;
+
+            Input = new InputModel
+            {
+                PhoneNumber = phoneNumber,
+                DarkMode = darkMode
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Nie uda³o siê za³adowaæ u¿ytkownika o ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Nie uda³o siê za³adowaæ u¿ytkownika o ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var darkMode = user.DarkMode;
+            if (Input.DarkMode != darkMode)
+            {
+                user.DarkMode = Input.DarkMode;
+                var setDarkModeResult = await _userManager.UpdateAsync(user);
+                if (!setDarkModeResult.Succeeded)
+                {
+                    StatusMessage = "Nieoczekiwany b³¹d w trakcie zmiany kolorystyki.";
+                    return RedirectToPage();
+                }
+            }
+
+
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    StatusMessage = "Nieoczekiwany b³¹d w trakcie ustawiania numeru telefonu.";
+                    return RedirectToPage();
+                }
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Twój profil zosta³ zaktualizowany!";
+            return RedirectToPage();
+        }
+    }
+}
