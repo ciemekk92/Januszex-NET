2421b9686bfb9dcc0405d163ac8fda41109a5a91
ciemekk92 <przemyslaw.reducha@gmail.com>
2021-11-26 23:09:40 +0100

    Frontend testing controller

--- Januszex/ClientApp/src/Modules/Home/Home.tsx
+++ Januszex/ClientApp/src/Modules/Home/Home.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import authService from '../api-authorization/AuthorizeService';
 
-export class Home extends React.Component {
+interface Props {}
+
+interface State {
+  name: string;
+  content: string;
+}
+
+export class Home extends React.Component<Props, State> {
   static displayName = Home.name;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      content: ''
+    };
+  }
+
+  onChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      ...this.state,
+      [target.name]: target.value
+    });
+  };
+
+  handleSubmit = async () => {
+    const token = await authService.getAccessToken();
+
+    const response = await fetch('api/Categories', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: !token
+        ? { 'Content-Type': 'application/json' }
+        : {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
+          }
+    });
+
+    const data = await response.json();
+    console.log({ data });
+  };
 
   render() {
     return (
@@ -11,6 +53,20 @@ export class Home extends React.Component {
           <li>Wyszukiwarka</li>
           <li>Kategorie</li>
           <li>Ogłoszenia promowane</li>
+          <br />
+          <input
+            name="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.onChange}
+          />
+          <input
+            name="content"
+            type="text"
+            value={this.state.content}
+            onChange={this.onChange}
+          />
+          <button onClick={this.handleSubmit}>KLIK</button>
         </ul>
       </div>
     );
