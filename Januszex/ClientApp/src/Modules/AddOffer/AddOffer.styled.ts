07cad43fea46a7da9fa0d7352d2d6b5dcc090cd8
ciemekk92 <przemyslaw.reducha@gmail.com>
2022-01-09 21:00:22 +0100

    feat/extend offer form

--- Januszex/ClientApp/src/Modules/AddOffer/AddOffer.styled.ts
+++ Januszex/ClientApp/src/Modules/AddOffer/AddOffer.styled.ts
@@ -3,12 +3,22 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: column;
   padding: 4rem;
+`;
+
+export const StyledRow = styled.div`
+  display: flex;
+
+  & h5 {
+    margin-top: 2rem;
+  }
 `;
 
 export const StyledColumn = styled.div`
   padding: 1rem;
   display: flex;
   width: 50%;
+  height: max-content;
   flex-direction: column;
 `;
--- Januszex/ClientApp/src/Modules/AddOffer/AddOffer.tsx
+++ Januszex/ClientApp/src/Modules/AddOffer/AddOffer.tsx
@@ -6,7 +6,11 @@ import { IOffer } from 'Types/stores';
 import { TextArea } from 'Shared/TextArea';
 import { FormField } from 'Shared/FormField';
 import { NumericInput } from 'Shared/NumericInput';
-import { StyledColumn, Wrapper } from './AddOffer.styled';
+import { Heading5 } from 'Shared/Typography';
+import { StyledColumn, StyledRow, Wrapper } from './AddOffer.styled';
+import { ButtonFilled } from '../../Shared/ButtonFilled';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../../Stores/Offer';
 
 export const AddOffer = (): JSX.Element => {
   const initialData: IOffer = {
@@ -37,6 +41,7 @@ export const AddOffer = (): JSX.Element => {
 
   const [inputData, setInputData] = React.useState<IOffer>(initialData);
 
+  const dispatch = useDispatch();
   const { t } = useTranslation();
 
   const onChange = ({
@@ -48,21 +53,93 @@ export const AddOffer = (): JSX.Element => {
     }));
   };
 
+  const onLocationChange = ({
+    target
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData((prevState: IOffer) => ({
+      ...prevState,
+      location: {
+        ...prevState.location,
+        [target.name]: target.value
+      }
+    }));
+  };
+
+  const onCityChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData((prevState: IOffer) => ({
+      ...prevState,
+      location: {
+        ...prevState.location,
+        city: {
+          ...prevState.location.city,
+          [target.name]: target.value
+        }
+      }
+    }));
+  };
+
+  const onRegionChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData((prevState: IOffer) => ({
+      ...prevState,
+      location: {
+        ...prevState.location,
+        region: {
+          ...prevState.location.region,
+          [target.name]: target.value
+        }
+      }
+    }));
+  };
+
+  const handleAddingOffer = async () => {
+    const result = await dispatch(actionCreators.createOffer(inputData));
+
+    console.log({ result });
+  };
+
   return (
     <Wrapper>
-      <StyledColumn>
-        <FormField label={t('addOffer.title')}>
-          <TextInput name="title" onChange={onChange} />
-        </FormField>
-        <FormField label={t('addOffer.price')}>
-          <NumericInput min={0} name="price" onChange={onChange} />
-        </FormField>
-      </StyledColumn>
-      <StyledColumn>
-        <FormField label={t('addOffer.content')}>
-          <TextArea name="content" onChange={onChange} />
-        </FormField>
-      </StyledColumn>
+      <StyledRow>
+        <StyledColumn>
+          <FormField label={t('addOffer.title')}>
+            <TextInput name="title" onChange={onChange} />
+          </FormField>
+          <FormField label={t('addOffer.price')}>
+            <NumericInput min={0} name="price" onChange={onChange} />
+          </FormField>
+        </StyledColumn>
+        <StyledColumn>
+          <FormField label={t('addOffer.content')}>
+            <TextArea name="content" onChange={onChange} />
+          </FormField>
+        </StyledColumn>
+      </StyledRow>
+      <StyledRow>
+        <Heading5>{t('addOffer.locationLabel')}</Heading5>
+      </StyledRow>
+      <StyledRow>
+        <StyledColumn>
+          <FormField label={t('addOffer.street')}>
+            <TextInput name="street" onChange={onLocationChange} />
+          </FormField>
+          <FormField label={t('addOffer.postalCode')}>
+            <TextInput name="postalCode" onChange={onLocationChange} />
+          </FormField>
+        </StyledColumn>
+        <StyledColumn>
+          <FormField label={t('addOffer.city')}>
+            <TextInput name="name" onChange={onCityChange} />
+          </FormField>
+          <FormField label={t('addOffer.region')}>
+            <TextInput name="name" onChange={onRegionChange} />
+          </FormField>
+        </StyledColumn>
+      </StyledRow>
+      <StyledRow>
+        <ButtonFilled onClick={handleAddingOffer}>
+          {t('addOffer.submit')}
+        </ButtonFilled>
+      </StyledRow>
     </Wrapper>
   );
 };
