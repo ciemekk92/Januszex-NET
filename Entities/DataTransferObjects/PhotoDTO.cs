76808f767218df2f0002487afc8944a624dae693
ciemekk92 <przemyslaw.reducha@gmail.com>
2022-01-14 00:26:00 +0100

    feat/add PhotoDTO

---
+++ Entities/DataTransferObjects/PhotoDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public class PhotoDTO
+    {
+        public string Id { get; set; }
+        public string Filename { get; set; }
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public string Link { get; set; }
+    }
+}
